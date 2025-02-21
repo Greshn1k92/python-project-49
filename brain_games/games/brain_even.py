0aea7ab26dd7e8675347f44b3e69c51ee4f91ee5
supersancho <sanmaximus92@mail.ru>
2025-02-21 09:26:40 +0300

    fix

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -35,5 +35,4 @@ def main():
     print(f"Congratulations, {name}!")
 
 
-if __name__ == "__main__":
-    main()
+
--- brain_games/games/brain_gcd.py
+++ brain_games/games/brain_gcd.py
@@ -19,9 +19,28 @@ def generate_question():
     return questiom, correct_answer
 
 def main():
-    generate_question()
+    print(RULES)
+    print("Welcome to the Brain Games!")
+    name = input("May I have your name? ")
+    print(f'Hello, {name}!')
 
-if __name__ == "__main__":
-    main()
+    correct_answer_count = 0
+
+    while correct_answer_count < 3:
+        question, correct_answer = generate_question()
+        print(f'Question: {question}')
+        user_answer = input("Your answer: ")
+
+        if user_answer == correct_answer:
+            print("Correct!")
+            correct_answer_count += 1
+        else:
+            print(f"'{user_answer}' is wrong answer ;(. "
+                  f"Correct answer was '{correct_answer}'.")
+            print(f"Let's try again, {name}!")
+            return
+
+    print(f"Congratulations, {name}!")
+
 
 
