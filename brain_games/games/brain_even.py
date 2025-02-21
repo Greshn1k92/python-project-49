19e55471160a8f4f69972144667b482d9bd62cf0
supersancho <sanmaximus92@mail.ru>
2025-02-21 09:01:37 +0300

    fix

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -14,7 +14,7 @@ def main():
     print("Welcome to the Brain Games!")
     name = input("May I have your name? ")
     print(f'Hello, {name}!')
-
+    print('Answer "yes" if the number is even, otherwise answer "no".')
     print(RULES)
 
     correct_answer_count = 0
